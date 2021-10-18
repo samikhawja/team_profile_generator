25026b41e0e8c69b3bb601c61ac209fa148f7cc2
samikhawja <skhawja11@gmail.com>
2021-10-17 17:01:27 -0700

    cleaned up files

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 const inquire = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateHTML = require('./utils/generateHTML');
 const inquirer = require('inquirer');
 
 const questions = [
@@ -47,7 +47,7 @@ function init() {
     .prompt(questions)
     .then((response) => {
         console.log(response)
-        writeToFile("index.html", generateMarkdown(response));
+        writeToFile("index.html", generateHTML(response));
     });
 }
 
